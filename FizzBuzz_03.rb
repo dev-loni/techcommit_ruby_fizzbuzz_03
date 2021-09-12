af51cc2fee2ffef1dca3041c24bf789356ca2310
dev-loni <90553218+dev-loni@users.noreply.github.com>
2021-09-12 20:52:00 +0900

    Update FizzBuzz_03.rb

--- FizzBuzz_03.rb
+++ FizzBuzz_03.rb
@@ -1,20 +1,7 @@
 for i in 1..150 do
-
     if (i%3==0 || i.to_s.include?("3"))
-        if i<10
-            puts "0#{i}!"
-        elsif i<100
-            puts "#{i}!"
-        else
-            puts i
-        end 
-    else
-        if i<10
-            puts "00#{i}"
-        elsif i<100
-            puts "0#{i}"
-        else
-            puts i
-        end
+        puts (i.to_s.rjust(2,'0') + '!').slice(0..2)
+    elsif
+        puts i.to_s.rjust(3,'0')
     end
 end
